fc5abe15f4b35d0651add937fa4794c22616f343
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-17 16:18:36 +0530

    basic analysis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Sentiment from 'sentiment';
+import { HttpClient } from '@angular/common/http';
+import { WORDS } from './words';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'xenon';
+  news: any;
+  final: any = [];
+  constructor(
+    private http: HttpClient
+  ){}
+
+
+  ngOnInit() {
+    const options = {
+      extras: WORDS
+    }
+    this.http.get('https://gnews-sc.herokuapp.com').subscribe( (data: any) => {
+      this.news = data.final;
+      this.news.map(item => {
+        const sentiment = new Sentiment();
+        if (sentiment.analyze(item.title._text, options).score > .5) {
+            this.final.push(item)
+        }
+      })
+    })
+  
+  }
 }
---
+++ src/app/words.ts
@@ -0,0 +1,23 @@
+export const WORDS = {
+    'covid-19': -1,
+    'covid': -1,
+    'verge': 2,
+    'new': 1,
+    'techcrunch': 2,
+    'informationweek': 1,
+    'forbes': .5,
+    'pandemic': -1,
+    'elon': 3,
+    'develop': 2,
+    'developement': 2,
+    'reopen': 1,
+    'helps': 2,
+    'help': 2,
+    'agony': -2,
+    'helping': 2,
+    'vaccine': 2,
+    'vaccination': 2,
+    'cyclone': -1,
+    'techrepublic': 2,
+    'nasa': 3
+}
