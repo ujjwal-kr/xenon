371fc16263d3f9f2a0b3e521aae9030eccb1292e
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-18 12:46:02 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +14,6 @@ import { StoryModule } from './story.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     NewsModule,
     StoryModule
   ],
