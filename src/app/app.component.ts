258063e3148d728935ffa1e04ca9c4610b17d27c
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-18 18:54:07 +0530

    added more features to timing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,29 +12,30 @@ export class AppComponent implements OnInit {
   title = 'xenon';
   news: any;
   final: any;
+  time: any = 'not yet Expired';
   constructor(
     private http: HttpClient
-  ){}
-// 3hr === 10800000ms
+  ) { }
+  // 3hr === 10800000ms
   async ngOnInit() {
     const options = {
       extras: WORDS
     }
     let payload = await JSON.parse(window.localStorage.getItem("news"));
-    if (payload) {
-      if (Date.now() - payload.time >= 3600000) {
-        window.localStorage.removeItem("news");
-        payload = null;
-      }
+    if (await payload) {
+        if (payload.expiry <= Date.now()) {
+            localStorage.removeItem("news");
+            payload = null;
+        }
     }
-    if (!payload){
-      this.http.get('https://gnews-sc.herokuapp.com').subscribe( (data: any) => {
+    if (!payload) {
+      this.http.get('https://gnews-sc.herokuapp.com').subscribe((data: any) => {
         this.news = data.final;
         this.final = [];
         this.news.map(item => {
           const sentiment = new Sentiment();
           if (sentiment.analyze(item.title._text, options).score > 1) {
-              this.final.push(item)
+            this.final.push(item)
           }
         })
         this.saveFinal();
@@ -47,7 +48,7 @@ export class AppComponent implements OnInit {
   saveFinal() {
     const data = {
       news: this.final,
-      time: Date.now()
+      expiry: Date.now() + 3600000
     }
     window.localStorage.setItem("news", JSON.stringify(data));
   }
