ef297d154cdacd78ba4be94f210f857283454b25
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-06-20 13:33:32 +0530

    added responsive stuff plus some components

--- src/app/news.module.ts
+++ src/app/news.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news/news.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { TopicsComponent } from './news/topics/topics.component';
+import { SearchComponent } from './news/search/search.component';
 
 const routes: Routes = [
-  { path: '', component: NewsComponent }
+  { path: '', component: NewsComponent },
+  { path: '', component: TopicsComponent }
 ]
 
 @NgModule({
-  declarations: [NewsComponent],
+  declarations: [NewsComponent, TopicsComponent, SearchComponent],
   imports: [
     CommonModule,
     HttpClientModule,
