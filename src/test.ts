41c5f49cf6bf36330d6cc8dc34615eb19d9728cf
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-08-09 17:26:32 +0530

    now

--- src/test.ts
+++ src/test.ts
@@ -21,5 +21,4 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
 context.keys().map(context);
