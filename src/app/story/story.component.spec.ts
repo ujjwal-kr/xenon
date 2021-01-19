fd0ebb15c93bfcf6f7af251b4a61439a81a189ac
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-01-19 23:30:04 +0530

    ng 11

--- src/app/story/story.component.spec.ts
+++ src/app/story/story.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StoryComponent } from './story.component';
 
@@ -6,7 +6,7 @@ describe('StoryComponent', () => {
   let component: StoryComponent;
   let fixture: ComponentFixture<StoryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StoryComponent ]
     })
